f3e2532ce08ddfc5ad49df2ad63ac2527ea28908
sudoxx2 <moungpeter@gmail.com>
2019-10-17 22:39:40 -0700

    added and created cookie api endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 var users = require('./routes/api/users')
 var bodyParser = require('body-parser')
+var session = require('express-session');
 
 const app = express();
 
@@ -13,6 +14,37 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 80000 }}))
+ 
+// Access the session as req.session
+app.get('/add_cookie', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session. refresh!')
+  }
+});
+
+app.get('/destroy_cookie', function(req, res, next) {
+    if(req.session.views) {
+        res.setHeader('Content-Type', 'text/html')
+        req.session.destroy();
+        res.end('session is destroyed');
+        
+    } else {
+        req.session.views = 1
+        res.end('welcome to the session. refresh!')
+    }
+
+});
 
 app.use('/users', users)
 
